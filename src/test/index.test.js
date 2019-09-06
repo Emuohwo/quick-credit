884d005c9f133e36f2a3c0a00b87bfb43b3ae8d0
Warri <warriisaac@outlook.com>
2019-09-06 22:57:19 +0100

    Add travis badge

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,9 +1,18 @@
-const chai = require( 'chai');
-const server = require ('../../server');
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../../server';
 
-
+const should = chai.should();
+const expect = chai.expect();
+chai.use(chaiHttp)
 describe('Get Home Route', () => {
     it('It should return Welcome to Quick Loan', () => {
-        expect('Welcome to Quick Credit').to.equal('Welcome to Quick Credit');
+        chai.request(server)
+        .get('/')
+        .end((err, res) => {
+            expect('Welcome to Quick Credit').to.equal('Welcome to Quick Credit');
+            res.should.have.status(200);
+            done();
+        })
     })
 })
